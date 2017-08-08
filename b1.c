0db9388743deb350c1061ae5319c0e9421baba65
PSrijeyanthi <30835237+PSrijeyanthi@users.noreply.github.com>
2017-08-08 19:49:31 +0530

    Create b1.c

---
+++ b1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if(num > 0)
+    {
+        printf("Number is POSITIVE");
+    }
+    else if(num < 0)
+    {
+        printf("Number is NEGATIVE");
+    }
+    else
+    {
+        printf("Number is ZERO");
+    }
+    return 0;
+}
